b44607faf14c0f43e52f6e3ba7f26d72daf61e47
Pawee Tantivasdakarn <pawee@alphainnovationslab.io>
2024-10-14 23:29:28 +0700

    add workable mvp

--- app/src/main.rs
+++ app/src/main.rs
@@ -3,13 +3,16 @@ use alloy::{
     providers::ProviderBuilder,
     transports::http::{Client, Http},
 };
-// use alloy_primitives::ruint::UintTryFrom;
 use eyre::Result;
 use foundry_contracts::iinitlens::IInitLens::{self, IInitLensInstance};
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    // constants
     const INIT_LENS_ADDRESS: &str = "0x4403F4296BeF042a08785077D67F4700478800C5";
+    const _INIT_CORE: &str = "0x972BcB0284cca0152527c4f70f8F689852bCAFc5";
+    const _POS_MANAGER: &str = "0x0e7401707CD08c03CDb53DAEF3295DDFb68BBa92";
+    const _SWAP_DATA_REGISTRY: &str = "0x94670598E98f8DAd95D85932dD85CBD050CE1402";
 
     // Spin up a forked Anvil node.
     // Ensure `anvil` is available in $PATH.
@@ -17,21 +20,26 @@ async fn main() -> Result<()> {
     let provider =
         ProviderBuilder::new().on_anvil_with_wallet_and_config(|anvil| anvil.fork(rpc_url));
 
+    // init lens instance
     let init_lens: IInitLensInstance<Http<Client>, _> =
         IInitLens::new(INIT_LENS_ADDRESS.parse::<Address>()?, provider.clone());
 
+    // example pos id
     let pos_id =
         uint!(13896239034349855609814759822748684436256448872554724177418048799807749705170_U256);
 
+    // create builder
     let builder = init_lens.getInitPosInfo(pos_id);
 
+    // call
     let data = builder.call().await?;
+
+    // destruct return data
     let info = data.posInfo;
     let health = info.health_e18;
-
     let health_string: String = format_units(health, 18)?;
 
+    // print Info
     println!("health: {health_string}");
-    // let blk = provider.get_block_number().await?;
     Ok(())
 }
