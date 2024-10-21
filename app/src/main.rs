b4b9f049f60fd4a457c920a4e2836d04ea951b2a
mewpawee <pawee@alphainnovationslab.io>
2024-10-21 19:22:06 +0700

    add get positions

--- app/src/main.rs
+++ app/src/main.rs
@@ -6,13 +6,20 @@ use alloy::{
 use eyre::Result;
 use foundry_contracts::iinitlens::IInitLens::{self, IInitLensInstance};
 
+mod positions;
+
 #[tokio::main]
-async fn main() -> Result<()> {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // constants
     const INIT_LENS_ADDRESS: &str = "0x4403F4296BeF042a08785077D67F4700478800C5";
     const _INIT_CORE: &str = "0x972BcB0284cca0152527c4f70f8F689852bCAFc5";
     const _POS_MANAGER: &str = "0x0e7401707CD08c03CDb53DAEF3295DDFb68BBa92";
     const _SWAP_DATA_REGISTRY: &str = "0x94670598E98f8DAd95D85932dD85CBD050CE1402";
+
+    // get active positions
+    let pos = positions::get_active_positions().await?;
+
+    println!("{:#?}", pos[0].viewer);
 
     // Spin up a forked Anvil node.
     // Ensure `anvil` is available in $PATH.
@@ -25,11 +32,10 @@ async fn main() -> Result<()> {
         IInitLens::new(INIT_LENS_ADDRESS.parse::<Address>()?, provider.clone());
 
     // example pos id
-    let pos_id =
-        uint!(13896239034349855609814759822748684436256448872554724177418048799807749705170_U256);
+    // let pos_id = uint!(pos[0].pos_id);
 
     // create builder
-    let builder = init_lens.getInitPosInfo(pos_id);
+    let builder = init_lens.getInitPosInfo(pos[0].pos_id);
 
     // call
     let data = builder.call().await?;
---
+++ app/src/positions.rs
@@ -0,0 +1,64 @@
+use serde::{Deserialize, Serialize};
+use serde_json::Number;
+use std::collections::{BTreeMap, HashMap};
+
+use alloy::primitives::{Address, U256};
+
+const BACKEND_API: &str = "https://index.init.capital/positions/positions";
+
+#[derive(Serialize, Deserialize)]
+pub struct RawData {
+    borrow_pool_tokens: HashMap<String, String>,
+    collateral_pool_tokens: HashMap<String, String>,
+    owner: String,
+    viewer: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct RawPositions {
+    data: BTreeMap<String, RawData>,
+    status_code: Number,
+}
+
+#[derive(Debug)]
+pub struct Position {
+    pub pos_id: U256,
+    pub borrow_pool_tokens: Vec<Address>,
+    pub collateral_pool_tokens: Vec<Address>,
+    pub owner: Address,
+    pub viewer: Address,
+}
+
+pub async fn get_active_positions() -> Result<Vec<Position>, Box<dyn std::error::Error>> {
+    let resp: RawPositions = reqwest::get(BACKEND_API).await?.json::<RawPositions>().await?;
+
+    let mut positions = Vec::<Position>::new();
+
+    for (key, value) in resp.data.iter() {
+        positions.push(Position {
+            pos_id: key.parse::<U256>()?,
+            borrow_pool_tokens: value
+                .borrow_pool_tokens
+                .keys()
+                .map(|x| x.parse::<Address>().unwrap())
+                .collect::<Vec<Address>>(),
+            collateral_pool_tokens: value
+                .collateral_pool_tokens
+                .keys()
+                .map(|x| x.parse::<Address>().unwrap())
+                .collect::<Vec<Address>>(),
+            owner: value.owner.parse::<Address>()?,
+            viewer: value.viewer.parse::<Address>()?,
+        });
+    }
+
+    // filter only active positions
+    let filtered_positions = positions
+        .into_iter()
+        .filter(|pos| pos.borrow_pool_tokens.len() > 0 && pos.collateral_pool_tokens.len() > 0)
+        .collect::<Vec<Position>>();
+
+    // println!("{:#?}", filtered_positions);
+
+    Ok(filtered_positions)
+}
